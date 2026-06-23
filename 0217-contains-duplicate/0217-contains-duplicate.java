5cc5a1e8a550db1b4baae8825cb8e7679e5e859f
Mohammad Ayan <ayanmd0617@gmail.com>
2026-06-24 01:02:13 +0530

    Time: 14 ms (89.2%), Space: 93.3 MB (60.06%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> a=new HashSet<>();
+        for(int i:nums){
+            if(a.contains(i))return true;
+            a.add(i);
+        }
+        return false;
+    }
+}
